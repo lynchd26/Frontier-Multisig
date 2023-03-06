71d69a383edb8138dcc8363e9005dc3531453150
Dara Lynch <dara.lynch63@mail.dcu.ie>
2023-03-06 18:32:52 +0000

    FRON-13 COMPLETE Created updateable divs without needing refresh.

---
+++ src/config.js
@@ -0,0 +1,1 @@
+// export const frontierMultisig = "0x....."
--- src/frontier-multisig/pages/index.js
+++ src/frontier-multisig/pages/index.js
@@ -2,33 +2,73 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+function IndexPage({ currentPage }) {
   return (
-    <>    
-      <main className={`${styles.main} rounded-lg`}>
-          <div className='md:columns-2 text-white' style={{height: '50%'}}>
-            <div className='column'>
-              <h1 className='text-4xl font-bold '>Hello World!</h1>
-              <p className='text-xl'>Hola mundo!</p>
-              </div>
-              <div className='column'>
-              <h1 className='text-4xl font-bold'>Hello World!</h1>
-              <p className='text-xl'>Hola mundo!</p>
-              </div>
-          </div><div className='md:columns-2 text-white'  style={{height: '50%'}}>
-            <div className='column'>
-            <h1 className='text-4xl font-bold'>Hello World!</h1>
-              <p className='text-xl'>Hola mundo!</p>
-              </div>
-              <div className='column'>
-              <h1 className='text-4xl font-bold'>Hello World!</h1>
-              <p className='text-xl'>Hola mundo!</p>
-              </div>
-          </div>
-      </main>
-    </>
-  )
+   <main className={`${styles.main} rounded-lg`}>
+      {currentPage === 'page1' ? (
+        
+        <div className='bg-green-400'>
+          <p>Your current balance is $123.03</p>
+        </div>
+
+      ) : null}
+      {currentPage === 'page2' ? (
+
+        <div className='bg-white'>
+          <p>This is the content of page 2.</p>
+        </div>
+
+      ) : null}
+      {currentPage === 'page3' ? (
+        
+        <div className='bg-red-500'>
+          <p>This is the content of page 3.</p>
+        </div>
+
+      ) : null}
+    </main>
+  );
 }
+
+IndexPage.getInitialProps = async ({ query }) => {
+  const initialPage = query.page || 'page1';
+  return { initialPage };
+};
+
+export default IndexPage;
+
+// export default function Home() {
+//   return (
+//     <>    
+//       <main className={`${styles.main} rounded-lg`}>
+//           <div className='md:columns-2 text-white' style={{height: '50%'}}>
+//             <div className='column'>
+//               <h1 className='text-4xl font-bold '>Hello World!</h1>
+//               <p className='text-xl'>Hola mundo!</p>
+//               </div>
+//               <div className='column'>
+//               <h1 className='text-4xl font-bold'>Hello World!</h1>
+//               <p className='text-xl'>Hola mundo!</p>
+//               </div>
+//           </div><div className='md:columns-2 text-white'  style={{height: '50%'}}>
+//             <div className='column'>
+//             <h1 className='text-4xl font-bold'>Hello World!</h1>
+//               <p className='text-xl'>Hola mundo!</p>
+//               </div>
+//               <div className='column'>
+//               <h1 className='text-4xl font-bold'>Hello World!</h1>
+//               <button>
+//                 <a href='./index2'>Google</a>
+//               </button>
+//               <p className='text-xl'>Hola mundo!</p>
+//               </div>
+//           </div>
+//       </main>
+//     </>
+//   )
+// }
