8a5c101dbf1bea73887dcdee511accf024e6cf8c
Dara <dara.lynch63@mail.dcu.ie>
2023-03-06 16:22:01 +0000

    FRON-12 PROGRESS Moved Add Wallet button to top bar.

--- src/frontier-multisig/pages/_app.js
+++ src/frontier-multisig/pages/_app.js
@@ -54,7 +54,23 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
-    <div className='flex items-center bg-gradient-to-r from-gray-100 via-gray-400 to-white dark:bg-gradient-to-r dark:from-black dark:via-zinc-800 to-black'>
+
+    <div>
+
+
+      <header class="fixed h-16 bg-indigo-500 shadow-md z-50 w-full px-5 py-2 flex justify-between items-center">
+        <router-link to="/" class="text-2xl pl-10 text-white">Fontier Multisig</router-link>
+        <div className="absolute right-0 overflow-y-auto rounded-lg border-4 border-r-8 border-indigo-500/25 shadow-xl bg-indigo-500 dark:bg-gray-800">            
+              <button className="space-y-2 justify-center items-center"
+                    onClick={() => connectWallet()}>
+                    <a href="#" className="flex items-center pl-6 pr-10 text-base transition duration-75 font-normal text-gray-100 rounded-lg dark:text-white hover:bg-indigo-700 dark:hover:bg-gray-700">                     
+                      <span className="ml-3 mr-3 ">{walletButtonText}</span>
+                    </a>
+              </button>
+        </div>
+      </header>
+    
+    <div className='flex items-center pt-16 bg-gradient-to-r from-gray-100 via-gray-400 to-white dark:bg-gradient-to-r dark:from-black dark:via-zinc-800 to-black'>
 
         <button on data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <span className="sr-only">Open sidebar</span>
@@ -64,7 +80,7 @@ function MyApp({ Component, pageProps }) {
         </button>
 
         <aside id="default-sidebar" className="fixed pl-10 pt-10 pb-10 z-40 w-1/5 h-screen transition-transform -translate-x-full sm:translate-x-0 align" aria-label="Sidebar">
-          <div className="flex flex-wrap px-3 py-4 overflow-y-auto rounded-lg p-4 border-4 border-indigo-500/25 shadow-xl bg-indigo-500 dark:bg-gray-800">            
+          <div className="items-center px-3 py-4 overflow-y-auto rounded-lg p-4 border-4 border-indigo-500/25 shadow-xl bg-indigo-500 dark:bg-gray-800">            
               <ul className="space-y-2 justify-center items-center">
                 <li>
                     <a href="#" className="flex items-center p-2 text-base transition duration-75 font-normal text-gray-100 rounded-lg dark:text-white hover:bg-indigo-700 dark:hover:bg-gray-700">
@@ -83,30 +99,18 @@ function MyApp({ Component, pageProps }) {
                     <a href="#" className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg dark:text-white hover:bg-indigo-700 dark:hover:bg-gray-700">
                       <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-50 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
                       <span className="flex-1 ml-3 whitespace-nowrap">Analytics</span>
-                      <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                      {/* <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
                     </a>
                 </li>
               </ul>
           </div>
-
-          <div className="b-2 absolute bottom-20 left-10 overflow-y-auto rounded-lg border-4 border-indigo-500/25 shadow-xl bg-indigo-500 dark:bg-gray-800">            
-                <button className="space-y-2 justify-center items-center"
-                      onClick={() => connectWallet()}>
-                      <a href="#" className="flex items-center pl-6 pr-10 text-base transition duration-75 font-normal text-gray-100 rounded-lg dark:text-white hover:bg-indigo-700 dark:hover:bg-gray-700">                     
-                        <span className="ml-3 mr-3 ">{walletButtonText}</span>
-                      </a>
-                </button>
-          </div>
         </aside>
       
-
-
-
-
       <div className="p-10 sm:ml-64 w-4/5 rounded-lg">
         <Component {...pageProps} />
       </div>
 
+    </div>
     </div>
     
   )
